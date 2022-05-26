12834a313c6ed1be7298f4ff3e80a54a6f56364c
AndreyHerzhov <andreyherzhov@gmail.com>
2022-05-26 13:16:09 +0300

    sameCase

--- js/8ky.js
+++ js/8ky.js
@@ -186,27 +186,27 @@
  * I love you, a little , a lot, passionately ... not at all
  */
 
-const arrayOfPhrases = ["I love you","a little","a lot", "passionately", "madly", "not at all"];
+// const arrayOfPhrases = ["I love you","a little","a lot", "passionately", "madly", "not at all"];
 
 // ----------- 1 variant 
-function howMuchILoveYou(nbPetals) {
-  return arrayOfPhrases[(nbPetals - 1) % arrayOfPhrases.length]
-}
+// function howMuchILoveYou(nbPetals) {
+//   return arrayOfPhrases[(nbPetals - 1) % arrayOfPhrases.length]
+// }
 
 // ---------- 2 variant 
-function howMuchILoveYou(nbPetals) {
-  if(nbPetals > 0 && nbPetals <= 6) {
-    const phrase = arrayOfPhrases[nbPetals - 1]
-   return phrase
-  } else if(nbPetals >= 7){
-    const newPhrase = arrayOfPhrases[nbPetals - 7]
-    return newPhrase
-  }
-}
+// function howMuchILoveYou(nbPetals) {
+//   if(nbPetals > 0 && nbPetals <= 6) {
+//     const phrase = arrayOfPhrases[nbPetals - 1]
+//    return phrase
+//   } else if(nbPetals >= 7){
+//     const newPhrase = arrayOfPhrases[nbPetals - 7]
+//     return newPhrase
+//   }
+// }
 
-howMuchILoveYou(7) // 0
-howMuchILoveYou(3) // 2
-howMuchILoveYou(6) // 5
+// howMuchILoveYou(7) // 0
+// howMuchILoveYou(3) // 2
+// howMuchILoveYou(6) // 5
 
 
 
@@ -235,8 +235,129 @@ howMuchILoveYou(6) // 5
 // console.log('10 % 6-', 10 % 6) // 4
 
 
+/*
+ * Freudian translator
+ */
+
+// const string = "This is a test"
+// const longerString = "This is a longer test"
+// const empty = "  "
+
+
+
+// function toFreud(string) {
+//   const arrayOfWords = string.split(' ')
+//   const stringOfWords = string.split(' ').join('')
+//   const numberOfWords = stringOfWords.length
+//   if(numberOfWords === 0) {
+//     return ''
+//   } else {
+//     const result = arrayOfWords.map(el => el = 'sex')
+//     return result;
+//   }
+   
+// }
+  
+// console.log(toFreud(string))
+// console.log(toFreud(longerString))
+// console.log(toFreud(empty))
+
+
+/*
+ * What is between?
+ */
+ 
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
 
  
 
+// function between(a, b) {
+//   let array = []
+//   for (let i = a; i <= b; i += 1) {
+//     array.push(i)
+//   }
+//  return array
+// }
 
+// console.log(between(1,4))
+
+
+
+/*
+ * Check same case
+ */
+
+
+// Examples
+// 'a' and 'g' returns 1
+
+// 'A' and 'C' returns 1
+
+// 'b' and 'G' returns 0
+
+// 'B' and 'g' returns 0
+
+// '0' and '?' returns -1
+
+
+function sameCase(a, b){
+  if (typeof a !== 'string' || typeof b !== 'string'){
+    return -1;
+  } else if(a.toLowerCase() === a &&  b.toLowerCase()  === b) {
+    return 1
+  }
+  else if(a.toUpperCase() === a &&  b.toUpperCase()  === b) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+// console.log(sameCase('A', '0'))
+// console.log(sameCase(1, 'S'))
+// console.log(sameCase('A', 'b'))
+
+
+// function checkForString(a,b) {
+//   if (typeof a !== 'string' || typeof b !== 'string'){
+//         return '-1';
+//       } else if(typeof a === 'string' && typeof b === 'string') {
+//         return 'Both strings'
+//       }
+// }
+
+// console.log(checkForString(1, 'd'))
+// console.log(checkForString(1, 2))
+// console.log(checkForString('1', 'd'))
+
+
+
+// function sameCase (a,b) {
+//   if (!(/[a-zA-Z]/).test(a) || !(/[a-zA-Z]/).test(b)){
+//     return -1;
+//   } else if((/[A-Z]/).test(a) && (/[A-Z]/).test(b)){
+//     return 1
+//   } else if((/[a-z]/).test(a)  && (/[a-z]/).test(b)) {
+//     return 1
+//   } else {
+//     return 0
+//   }
+// }
+
+// console.log(sameCase('a', 'b')) // 1
+// console.log(sameCase('A', 'C')) // 1
+// console.log(sameCase('b', 'G')) // 0
+// console.log(sameCase('B', 'B')) // 1
+// console.log(sameCase(0, '?')) // -1
  
+
+// console.log((/[a-zA-Z]/).test('A'))
+// console.log((/[a-zA-Z]/).test('b'))
+// console.log((/[a-zA-Z]/).test(1))
+
+
+/*
+ *  
+ */
