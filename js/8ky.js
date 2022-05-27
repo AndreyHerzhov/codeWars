185ec2363912d1d394e74b8ed8e9c9fa0ef6f93e
AndreyHerzhov <andreyherzhov@gmail.com>
2022-05-27 08:24:49 +0300

    water

--- js/8ky.js
+++ js/8ky.js
@@ -359,5 +359,33 @@ function sameCase(a, b){
 
 
 /*
- *  
+ *  How much water do I need?
  */
+// My washing machine is an old model that can only handle double the amount of load (or max_load). 
+// If the amount of clothes is more than 2 times the standard amount of load (max_load),
+//  return 'Too much clothes'. 
+// The washing machine also cannot handle any amount of clothes less than load (max_load). 
+// If that is the case, return 'Not enough clothes'.
+
+function howMuchWater(water, load, clothes){
+   if(clothes > load * 2){
+     return 'Too much clothes'
+   } else if(clothes < load) {
+     return 'Not enough clothes'
+   } else {
+     const result =   water * Math.pow(1.1, clothes - load) 
+     const resultToFixed = result.toFixed(2) // add + before
+     return resultToFixed
+   }
+   
+} 
+
+
+
+
+console.log(howMuchWater(10,10,21))
+console.log(howMuchWater(10,10,2))
+console.log(howMuchWater(10,11,20))
+console.log(typeof (howMuchWater(50,15,29))) // string
+
+ 
